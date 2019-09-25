5f3e3f68636c44f3b6cb61d2457a41c9b373053e
gagan taneja <tanejagagan@gagans-MacBook-Pro.local>
2019-09-25 12:56:38 -0700

    Add no iteration benchmark

--- src/main/scala/ReadTestData.scala
+++ src/main/scala/ReadTestData.scala
@@ -1,4 +1,4 @@
-import redis.clients.jedis.StreamEntryID
+import redis.clients.jedis.{Jedis, StreamEntryID}
 
 object ReadTestData {
 
@@ -9,6 +9,7 @@ object ReadTestData {
     val iteration = args(2).toInt
     implicit val redisConfig : RedisConfig = new RedisConfig( RedisEndpoint(auth = "passwd"))
     readData(redisConfig, streamKey, batchSize, iteration)
+    readNoIterate(redisConfig, streamKey, batchSize, iteration)
     System.exit(0)
   }
 
@@ -17,6 +18,17 @@ object ReadTestData {
       val data = RedisStreamReader.getDataNG(redisConfig, streamKey, new StreamEntryID(0, 0),
         new StreamEntryID(Long.MaxValue, Long.MaxValue), batchSize)
       data.count(x => true)
+    })
+  }
+
+  def readNoIterate(redisConfig: RedisConfig, streamKey : String, batchSize : Int, iteration : Int): Unit = {
+    val t3 = BenchmarkUtil.time(iteration, {
+      ConnectionUtils.withConnection(redisConfig.connectionForKey(streamKey)) { conn: Jedis =>
+        val client = conn.getClient
+        client.xrange(streamKey, new StreamEntryID(0, 0),
+          new StreamEntryID(Long.MaxValue, Long.MaxValue), batchSize)
+        client.getObjectMultiBulkReply()
+      }
     })
   }
 }
